92bb78ec6223e68e35cef11e31f7b03781003665
sashabileckij <bileckijsasa@gmail.com>
2017-11-07 00:53:51 +0300

    Вигляд меню на сторінці

---
+++ src/main/java/ua/ivfr/it/lms/views/ViewLoginServlet.java
@@ -0,0 +1,23 @@
+package ua.ivfr.it.lms.views;
+
+public class ViewLoginServlet {
+    public static String getLogin() {
+        return "<div class=\"container\">\n" +
+                "\n" +
+                "      <form class=\"form-signin\" role=\"form\">\n" +
+                "        <h2 class=\"form-signin-heading\">Вхід</h2>\n" +
+                "        <label for=\"inputEmail\" class=\"sr-only\">Імейл</label>\n" +
+                "        <input type=\"email\" id=\"inputEmail\" class=\"form-control\" placeholder=\"Імейл\" required autofocus>\n" +
+                "        <label for=\"inputPassword\" class=\"sr-only\">Пароль</label>\n" +
+                "        <input type=\"password\" id=\"inputPassword\" class=\"form-control\" placeholder=\"Пароль\" required>\n" +
+                "        <div class=\"checkbox\">\n" +
+                "          <label>\n" +
+                "            <input type=\"checkbox\" value=\"remember-me\"> Запам'ятати мене\n" +
+                "          </label>\n" +
+                "        </div>\n" +
+                "        <button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Зареєструватись</button>\n" +
+                "      </form>\n" +
+                "\n" +
+                "    </div> <!-- /container -->";
+    }
+}
