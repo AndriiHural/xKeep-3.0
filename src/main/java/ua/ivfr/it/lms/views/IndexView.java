242869e886890b91d8f97da63411f67336fa3303
serhii lypak <sergiy1297>
2017-12-01 17:18:00 +0200

    #правка до виводу поширених нотаток + метод що дасть змогу поширювати нотатки)

--- src/main/java/ua/ivfr/it/lms/views/IndexView.java
+++ src/main/java/ua/ivfr/it/lms/views/IndexView.java
@@ -1,9 +1,13 @@
 package ua.ivfr.it.lms.views;
 
+import ua.ivfr.it.lms.dao.SharedNotesDao;
+import ua.ivfr.it.lms.dao.SharedNotesDaoImp;
 import ua.ivfr.it.lms.models.Note;
+import ua.ivfr.it.lms.models.User;
 
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Виводить HTML сторінку bootstrap
@@ -44,16 +48,16 @@ public class IndexView {
         out.println(pathHtmlSingleton.getUser());
     }
 
-    public void doShareNotes(PrintWriter out){
+    public void doShareNotes(PrintWriter out) {
         out.println(pathHtmlSingleton.getDoshare());
     }
 
-    public void doPostShareNotes(PrintWriter out){
+    public void doPostShareNotes(PrintWriter out) {
         out.println(pathHtmlSingleton.getPostshare());
     }
+
     // виводить поле для вводу тексту нотатки/заголовка
     // і список всіх створених користувачем нотаток
-    //TODO добавити вивід нотаток розшарених іншими користувачати
     public void outNotePage(PrintWriter out, List<Note> notes) {
         out.println(pathHtmlSingleton.getNote_page());
         //при створенні нотатки в посилання іконки "видалити(х)" передаємо пареметер
@@ -62,10 +66,11 @@ public class IndexView {
             //TODO bg-note-? створити класи для кожного кольру
             out.println("<div id=replaceID class=\"col-xs-4  col-md-3 col-lg-3 col-sm-12 note-merg\">\n" +
                     "    <div class=\"bg-note-1 note-size remove-hover\">\n" +
-                    "        <a id=remove-item href=\"/note/delete?id="+ob_note.getId()+"\"><span class=\"glyphicon glyphicon-remove\"></span></a>\n" +
+                    "        <a id=remove-item href=\"/note/delete?id=" + ob_note.getId() + "\"><span class=\"glyphicon glyphicon-remove\"></span></a>\n" +
+                    "        <a id=share-item href=\"/note/share?id=" + ob_note.getId() + "\"><span class=\"glyphicon glyphicon-share-alt\"></span></a>\n" +
                     "        <a href=\"#\" data-toggle=\"modal\" data-target=\"#myModal\">\n" +
-                    "            <h2>Title #1 "+ob_note.getNote_title()+"</h2>\n" +
-                    "            <p>"+ob_note.getNote()+"</p>\n" +
+                    "            <h2>Title #1 " + ob_note.getNote_title() + "</h2>\n" +
+                    "            <p>" + ob_note.getNote() + "</p>\n" +
                     "        </a>\n" +
                     "\n" +
                     "    </div>\n" +
@@ -73,10 +78,9 @@ public class IndexView {
         }
         out.println("</div>" +
                 "</div><!--/body note-page -->");
-
-
-
     }
+
+
 }
 
 
